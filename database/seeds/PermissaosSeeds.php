3430b48e8137e085ae14bd0d2065430ef1ff19e8
Michel <michel.bolzon123@gmail.com>
2018-01-15 22:13:18 -0200

    michel

--- database/seeds/PermissaosSeeds.php
+++ database/seeds/PermissaosSeeds.php
@@ -3,7 +3,7 @@
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
 
-class PermissaoSeeds extends Seeder
+class PermissaosSeeds extends Seeder
 {
     /**
      * Run the database seeds.
