18c76d215906a2bff74b2bbb85b990495d548ac5
Michel <michel.bolzon123@gmail.com>
2018-01-16 13:25:46 -0200

    michel

--- resources/views/site/site.blade.php
+++ resources/views/site/site.blade.php
@@ -4,6 +4,7 @@
     <div class="container">
         <div class="row">
             <div class="col s12 m6">
+                <span>{{Auth::user()}}</span>
                 <div class="card blue darken-1">
                     <div class="card-content white-text">
                         <span class="card-title">Alunos</span>
